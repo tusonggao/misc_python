1ad03d71cd67df7d5ebe79ea94974f8eaaa7d137
Song-gao Tu <tusonggao@163.com>
2017-09-12 20:54:15 +0800

    Update digit_recognizer.py

--- digit_recognizer.py
+++ digit_recognizer.py
@@ -111,7 +111,7 @@ if __name__=='__main__':
 #                               n_jobs=4).fit(
 #                               train_data_frame[train_cols],
 
-###########################################################################
+
 
     gbc = GradientBoostingClassifier(n_estimators=2000,
                                      random_state=42,
