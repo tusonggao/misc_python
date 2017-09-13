7030ac692f8f5ab9c094aff834737a57db49a874
Song-gao Tu <tusonggao@163.com>
2017-09-13 23:47:12 +0800

    Update digit_recognizer_tf.py

--- digit_recognizer_tf.py
+++ digit_recognizer_tf.py
@@ -76,8 +76,7 @@ def main(_):
    
     for _ in range(1000):
         batch_xs, batch_ys = mnist.train.next_batch(100)
-        sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
-    
+        sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})    
    
     correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
