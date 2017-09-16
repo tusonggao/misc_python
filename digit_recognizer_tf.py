ab1bb0eca85eaef830a8c651567b961c8c378e63
Song-gao Tu <tusonggao@163.com>
2017-09-16 23:40:57 +0800

    Update digit_recognizer_tf.py

--- digit_recognizer_tf.py
+++ digit_recognizer_tf.py
@@ -158,6 +158,7 @@ if __name__=='__main__':
 #                                            train_data_frame['SalePrice'], 
 #                                            random_state=42)
 
+
 ######################################################################
             
 #    lr = LinearRegression().fit(X_train, y_train)
