543620f17ff8eeaaf341ae16f84dfc7ee2c84081
Song-gao Tu <tusonggao@163.com>
2017-09-18 23:10:37 +0800

    Update digit_recognizer_tf.py

--- digit_recognizer_tf.py
+++ digit_recognizer_tf.py
@@ -68,7 +68,7 @@ def main(_):
     # Define loss and optimizer
     y_ = tf.placeholder(tf.float32, [None, 10])
 
-    #
+    
     cross_entropy = tf.reduce_mean(
         tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
     train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
