5f3833a43000fce9191709fb5f20bb51dca97a6f
Song-gao Tu <tusonggao@163.com>
2017-10-07 21:39:08 +0800

    Update digit_recognizer_tf.py

--- digit_recognizer_tf.py
+++ digit_recognizer_tf.py
@@ -101,7 +101,7 @@ if __name__=='__main__':
     print('test_data_frame shape is ', test_data_frame.shape)
     
 
-#                         {'Survived': [1 if x==True else 0 for x in guess_outcome]}, 
+#                         {'Survived: [1 if x==True else 0 fo x in guess_outcome]}, 
 #                         index=test_data_frame.index.values)
 #    outcome_data_frame.to_csv('./tsg_outcome.csv', index_label='PassengerId')
 
