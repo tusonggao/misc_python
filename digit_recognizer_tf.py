a350d580727603b9a96bf0b984edffa19e3c3915
Song-gao Tu <tusonggao@163.com>
2017-10-16 20:15:09 +0800

    Update digit_recognizer_tf.py

--- digit_recognizer_tf.py
+++ digit_recognizer_tf.py
@@ -104,7 +104,6 @@ if __name__=='__main__':
 #                         index=test_data_frame.index.values)
 #    outcome_data_frame.to_csv('./tsg_outcome.csv', index_label='PassengerId')
 
-
 #    end_time = time.time()    
 #    print('time cost is ', end_time - start_time)    
 #    sys.exit(0)    
