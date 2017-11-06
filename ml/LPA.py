6576c065e9ba9b2aaeb5d0525534c4516cdf5a4d
yushan007 <tusonggao@163.com>
2017-11-06 15:57:32 +0800

    add LPA.py

---
+++ ml/LPA.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+points  = np.array([[2, 1],
+                    [3, 1],
+                    [4, 1],
+                    [3, 2],
+                    [4, 3],
+                    [100, 100]])
+
+num_points = len(points)
+
+print('num_points is ', num_points)
+
+x = points[:, 0]
+y = points[:, 1]
+print('x is ', x)
+print('y is ', y)
+
+w = 0
+b = 0
+print('np.sum(w*x+b-y) is ', np.sum(w*x+b-y))
+
+current_parameter = {'w': 0.0, 'b': 0.0}
+
+#learning_rate = 2.0
+learning_rate = 0.0002
+
+count = 0
+while True:
+    count += 1
+#    if count > 20:
+#        break
+    print('round is ', count)
+    w = current_parameter['w']
+    b = current_parameter['b']
+    print('w is ', w, 'b is ', b)
+    step_w = 2./num_points*np.sum(np.dot((w*x+b-y), x))
+    step_b = 2./num_points*np.sum(w*x+b-y)
+    current_parameter['w'] = w - learning_rate*step_w
+    current_parameter['b'] = b - learning_rate*step_b
+    delta = max(abs(step_w), abs(step_b))
+    print('delta is ', delta)
+    if delta < 10**(-4):
+        break
