a1c37c1641631e291226763ca4a49c6870eb7b4c
涂松高 <tusonggao@163.com>
2018-08-20 23:48:25 +0800

    change solver code

--- sudoku_solver/solver.py
+++ sudoku_solver/solver.py
@@ -14,24 +14,35 @@ file_name = 'C:/github_base/misc_python/sudoku_solver/input.txt'
 input_matrix = read_input(file_name)
 print('input_matrix is ', input_matrix)
 
-solve(0, 0, input_matrix)
+input_matrix_new = input_matrix.copy()
+print('input_matrix_new is ', input_matrix_new)
 
+#solve(0, 0, input_matrix)
+
+FOUND = False
 
 def next_position(i, j):
     num = i*9 + j + 1
     return divmod(num, 9)
 
+def prev_position(i, j):
+    num = i*9 + j - 1
+    return divmod(num, 9)
+
 
 def solve(i, j, input_matrix):
+    if Found:
+        return
     right_placement = check(i, j, input_matrix)
     if i==8 and j==8 and right_placement:
         print('found one solution', input_matrix)
-    else if right_placement:
-        solve()
+        Found = True
+    elif right_placement:
+        solve(i, j, input_matrix)
         
-for pos in range(9*9):
-    i, j = divmod(pos, 9)
-    print(i, j, input_matrix[i][j])
-    for val in range(1, 10):
+#for pos in range(9*9):
+#    i, j = divmod(pos, 9)
+#    print(i, j, input_matrix[i][j])
+        
         
 			    
