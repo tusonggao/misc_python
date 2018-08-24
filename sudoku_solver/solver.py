f2af2766fa917c62cffe96db599997bb77c10335
涂松高 <tusonggao@163.com>
2018-08-24 21:12:54 +0800

    update solver.py

--- sudoku_solver/solver.py
+++ sudoku_solver/solver.py
@@ -1,22 +1,35 @@
 from collections import Counter
-
-FOUND = False
+import copy
 
 def read_input(file_name):
-    input_matrix = []
+    local_input_matrix = []
     with open(file_name) as file:
         for line in file:
             line = line.strip()
             if len(line)==0:
                 continue
             list_n = [s for s in line.strip() if s!=' ']
-            input_matrix.append(list_n)
-    return input_matrix
+            local_input_matrix.append(list_n)
+    return local_input_matrix
 	
 def print_outcome(input_matrix):
     for i in range(9):
         print(' '.join([str(v) for v in input_matrix[i]]))
     print('\n---------------------------\n')
+
+FOUND = False
+
+file_name = 'C:/github_base/misc_python/sudoku_solver/input2.txt'
+input_matrix_original = read_input(file_name)
+print('input_matrix is ', input_matrix_original)
+print_outcome(input_matrix_original)
+
+input_matrix = copy.deepcopy(input_matrix_original)
+print_outcome(input_matrix)
+
+#input_matrix[3][3] = 100
+#print_outcome(input_matrix_original)
+
 
 def valid_position(i, j):
     return 0 <= i <= 8 and 0 <= j <=8
@@ -30,42 +43,50 @@ def prev_position(i, j):
     return divmod(num, 9)
 
 def check_ok(i, j, input_matrix):
-    print('in checkok i, j:', i, j, 'current value is ', input_matrix[i][j])
+#    print('in checkok i, j:', i, j, 'current value is ', input_matrix[i][j])
     ll1 = input_matrix[i]
-    print('ll1 is ', ll1)
     counter = Counter(ll1)
     for val, cnt in counter.items():
         if cnt>=2 and val!='X':
-            print('check_ok is ', False)
+#            print('ll1 is ', ll1)
+#            print('check_ok is ', False)
             return False
     
     ll2 = [input_matrix[x][j] for x in range(9)]
-    print('ll2 is ', ll2)
     counter = Counter(ll2)
     for val, cnt in counter.items():
         if cnt>=2 and val!='X':
-            print('check_ok is ', False)
+#            print('ll2 is ', ll2)
+#            print('check_ok is ', False)
             return False
         
     ll3 = [input_matrix[x][y] for x in range(i//3*3, i//3*3 + 3) 
                               for y in range(j//3*3, j//3*3 + 3)]
     counter = Counter(ll3)
-    print('ll3 is ', ll3)
+    
     for val, cnt in counter.items():
         if cnt>=2 and val!='X':
-            print('check_ok is ', False)
+#            print('ll3 is ', ll3)
+#            print('check_ok is ', False)
             return False
     
-    print('check_ok is ', True)
+#    print('check_ok is ', True)
     return True
 
 
 def solve(i, j, input_matrix):
-    global FOUND
-    if FOUND:
+    global FOUND, input_matrix_original
+    if FOUND==True:
+        print('already found one, i will exit')
         return
     
+    if valid_position(i, j)==False: 
+        return
+    
+#    print('in solve input_matrix_original is ', input_matrix_original)
+#    print('in solve input_matrix is ', input_matrix)
     while input_matrix_original[i][j]!='X':
+#        print('yes checked here ', i, j)
         i, j = next_position(i, j)
         if valid_position(i, j)==False: 
             return
@@ -74,27 +95,23 @@ def solve(i, j, input_matrix):
     for val in range(1, 10):
         input_matrix[pos_x][pos_y] = str(val)
         if check_ok(pos_x, pos_y, input_matrix):
-            print_outcome(input_matrix)
+#            print_outcome(input_matrix)
 #            name = input("please input：");
-            if i==8 and j==8:
+            if pos_x==8 and pos_y==8:
                 print('ok, found one')
                 print_outcome(input_matrix)
                 FOUND = True
             else:
                 i, j = next_position(i, j)
                 solve(i, j, input_matrix)
+        
         if input_matrix_original[pos_x][pos_y]=='X':
+#            print('yes got here', pos_x, pos_y)
             input_matrix[pos_x][pos_y] = 'X'
             
 
-file_name = 'C:/github_base/misc_python/sudoku_solver/input2.txt'
-input_matrix_original = read_input(file_name)
-print('input_matrix is ', input_matrix_original)
-print_outcome(input_matrix_original)
 
-input_matrix_new = input_matrix_original.copy()
-#print('input_matrix_new is ', input_matrix_new)
 
-#solve(0, 0, input_matrix_new)
+solve(0, 0, input_matrix)
        
 			    
